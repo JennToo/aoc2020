6f08b4efbaf0b61474e7b6b46784a9b191a16257
Jennifer Wilcox <jennifer@nitori.org>
2020-12-03 18:56:00 -0600

    Implement day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,3 +1,32 @@
+use aoc2020::read_lines;
+use itertools::Itertools;
+
 fn main() {
-    println!("Day 1");
+    let lines = read_lines("data/day1_input.txt").expect("Could not read input file");
+    let items = lines
+        .iter()
+        .map(|line| line.parse::<i64>().expect("Invalid number"))
+        .collect::<Vec<i64>>();
+    let part1: i64 = find_items_that_sum_to(&items, 2, 2020)
+        .unwrap()
+        .iter()
+        .product();
+    let part2: i64 = find_items_that_sum_to(&items, 3, 2020)
+        .unwrap()
+        .iter()
+        .product();
+    println!("Part 1: {:?}", part1);
+    println!("Part 2: {:?}", part2);
+}
+
+fn find_items_that_sum_to(
+    items: &Vec<i64>,
+    selection_count: usize,
+    sum_to: i64,
+) -> Option<Vec<i64>> {
+    items
+        .iter()
+        .cloned()
+        .combinations(selection_count)
+        .find(|candidate| candidate.iter().sum::<i64>() == sum_to)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::fs::File;
+use std::io::{self, prelude::*};
+use std::path::Path;
+
+pub fn read_lines<P: AsRef<Path>>(path: P) -> io::Result<Vec<String>> {
+    let file = File::open(path)?;
+    Ok(io::BufReader::new(file)
+        .lines()
+        .collect::<io::Result<Vec<String>>>()?)
 }
